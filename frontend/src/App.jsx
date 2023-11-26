58d4f5ef78c2fc699a5c7e5a2c0a4e70bf970428
Tammam Alhalabe <tammam@Tammams-Air.fritz.box>
2023-11-26 21:17:35 +0100

    die Komponant CreateRecipe wurde in den App.jsx verlinkt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./RecipeList";
-// import CreateRecipe from "./CreateRecipe";
+import CreateRecipe from "./CreateRecipe";
 // import RecipeDetails from "./RecipeDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,8 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<RecipeList />} />
-          {/* <Route path="/add-recipe" element={<CreateRecipe />} />
-          <Route path="/recipe-details/:recipeId" element={<RecipeDetails />} /> */}
+          <Route path="/add-recipe" element={<CreateRecipe />} />
+          {/* <Route path="/recipe-details/:recipeId" element={<RecipeDetails />} /> */}
         </Routes>
       </Router>
       ;
