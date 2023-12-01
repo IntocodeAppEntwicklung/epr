d8b22a16f8cd82312488c3c60bcafe687b5810ef
Tammam Alhalabe <tammam@Tammams-Air.fritz.box>
2023-12-01 09:35:27 +0100

    die Funktion RecipesProvider wurde in App.jsx implemntiert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./RecipeList";
 import CreateRecipe from "./CreateRecipe";
 import RecipeDetails from "./RecipeDetails";
+import { RecipesProvider } from "./RecipeContext.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/add-recipe" element={<CreateRecipe />} />
-          <Route path="/recipe-details/:recipeId" element={<RecipeDetails />} />
-        </Routes>
-      </Router>
-      ;
+      <RecipesProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<RecipeList />} />
+            <Route path="/add-recipe" element={<CreateRecipe />} />
+            <Route
+              path="/recipe-details/:recipeId"
+              element={<RecipeDetails />}
+            />
+          </Routes>
+        </Router>
+      </RecipesProvider>
     </>
   );
 }
